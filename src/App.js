c52fedce57588fd59da8917c542d10a58978e75a
Rony <frrony1996@gmail.com>
2022-04-29 00:07:12 +0500

    all page created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <ToastContainer />
     </div>
   );
 }
